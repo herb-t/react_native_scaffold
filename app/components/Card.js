7cb41715a8a50ec923418f2ba80a0e934323416a
Brad Murphy <brad.murphy@byhook.com>
2017-08-01 11:28:30 -0400

    Tidying up animation.  Adding rotate animation as well.

--- app/components/Card.js
+++ app/components/Card.js
@@ -8,7 +8,8 @@ import {
   Image,
   View,
   PanResponder,
-  Animated
+  Animated,
+  Easing
 } from 'react-native';
 import '../data.js';
 
@@ -24,54 +25,96 @@ const styles = StyleSheet.create({
 
 // Create Card class to handles swipe behavior and
 // to pass data through.
-export default class Card extends Component { 
+export default class Card extends Component {
+
   constructor(props) {
     // Define state and pass through props.
     super(props);
     this.state = {
-      xPosition: new Animated.Value(0),
-      yPosition: new Animated.Value(0)
+      pan: new Animated.ValueXY(),
+      scale: new Animated.Value(1),
+      rotate: new Animated.Value(0)
     }
+  }
 
+  componentWillMount() {
     // Create pan responder to fire animations on
     // swipe left and right.
+    const threshold = 120;
+
     this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: (evt, gestureState) => true,
-      onPanResponderRelease: (evt, gestureState) => {
-        if(gestureState.dx > 100) {
-          this.animateCard_(500, 350);
-        } else if (gestureState.dx < -100) {
-          this.animateCard_(-500, 350);
-        }
-      }
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+      onPanResponderGrant: (e, gestureState) => this.animateAndSetPanValues_(),
+      onPanResponderMove: Animated.event([
+        // When we drag/pan the object,
+        // set the delate to the states pan position.
+        null, {dx: this.state.pan.x, dy: this.state.pan.y},
+      ]),
+      onPanResponderRelease: (e, gestureState) => this.gaugeRelease_(gestureState, threshold)
     });
   }
 
-  animateCard_(x, y) {
-    // Animate cards based on coordinates passed.
+  animateAndSetPanValues_() {
+    // Set the initial value to the current state
+    this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
+    this.state.pan.setValue({x: 0, y: 0});
+    Animated.spring(
+      this.state.scale,
+      { toValue: 1.1, friction: 3 }
+    ).start();
     Animated.timing(
-      this.state.xPosition,
+      this.state.rotate,
       {
-        toValue: x,
-        duration: 500,
+        toValue: 1,
+        duration: 500
       }
     ).start();
+  }
 
+  animateRelease_(x, y) {
+    // Flatten the offset to avoid erratic behavior
+    this.state.pan.flattenOffset();
+    Animated.spring(
+      this.state.scale,
+      { toValue: 1, friction: 3 }
+    ).start();
+    Animated.spring(
+      this.state.pan,
+      { toValue: {x: x, y: y}, tension: 10 }
+    ).start();
     Animated.timing(
-      this.state.yPosition,
+      this.state.rotate,
       {
-        toValue: y,
-        duration: 500,
+        toValue: 0,
+        duration: 500
       }
     ).start();
+  }
+
+  gaugeRelease_(pos, threshold) {
+    // Gauge release movement threshold and animate accordingly.
+    if(pos.dx <= threshold && pos.dx >= -threshold) {
+      this.animateRelease_(0, 0);
+    } else if (pos.dx > threshold) {
+      this.animateRelease_(500, 350);
+    } else if (pos.dx < -threshold) {
+      this.animateRelease_(-500, 350);
+    }
   }
 
   render() {
     // Render card with animation styles and data hooked up.
-    let { xPosition, yPosition } = this.state;
+    const rotate = this.state.rotate.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '25deg']
+    });
+    let { pan, scale} = this.state;
+    let [translateX, translateY] = [pan.x, pan.y];
+    let imageStyle = {transform: [{translateX}, {translateY}, {rotate}, {scale}]};
 
     return (
-      <Animated.View {...this._panResponder.panHandlers} style={{...this.props.style, transform: [{translateX: xPosition}, {translateY: yPosition}]}}>
+      <Animated.View style={{...this.props.style, ...imageStyle}} {...this._panResponder.panHandlers}>
         <Image source={this.props.src} />
         <Text style={styles.label}>{this.props.name}</Text>
       </Animated.View>
