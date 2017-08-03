37a2c79ad884d6bff3b74430fae9c6c73f02b12a
Brad Murphy <brad.murphy@byhook.com>
2017-08-03 11:33:43 -0400

    Making app only portrait for android (this is how tinder works) and refactoring/commenting card component.  Also adding better naming conventions and variables.

--- app/components/Card.js
+++ app/components/Card.js
@@ -15,6 +15,7 @@ import styles from '../styles/styles.js';
 
 /**
  * Card Class to pass data through and handle swipe gestures and animations.
+ * @class
  */
 export default class Card extends Component {
 
@@ -24,35 +25,36 @@ export default class Card extends Component {
    * @struct
    */
   constructor(props) {
-    // Define state and pass in props for component.
-    // State includes animated pan, scale and rotate values as
-    // well as a threshold value.
+    // Pass through props.
     super(props);
+
+    // Define state with animated values and default thresholds.
     this.state = {
       pan: new Animated.ValueXY(),
       scale: new Animated.Value(1),
       rotate: new Animated.Value(0.5),
-      threshold: 0,
-      middle: 0
+      xThresh: 0,
+      yThresh: 0
     }
   }
 
   /**
    * Once the component is mounted, set up panResponder to
    * track the current coordinates for touch.
+   * @public
    */
   componentWillMount() {
     // Define height and width of window.
-    // Assign threshold and middle values.
+    // Assign xThresh and yThresh values.
     let { height, width } = Dimensions.get('window');
-    this.setState({threshold: width/4, middle: height/2});
+    this.setState({xThresh: width/4, yThresh: height/2});
 
     // Create pan responder to fire animations on
     // swipe left and right.
     this._panResponder = PanResponder.create({
       onMoveShouldSetResponderCapture: () => true,
       onMoveShouldSetPanResponderCapture: () => true,
-      onPanResponderGrant: (e, gestureState) => this.animateAndSetPanValues_(),
+      onPanResponderGrant: (e, gestureState) => this.animateResponse_(),
       onPanResponderMove: (e, gestureState) => {
         // When we drag/pan the object,
         // set the delate to the states pan position.
@@ -62,17 +64,18 @@ export default class Card extends Component {
 
         // Define rotate values to switch dependent upon
         // the current x position.
-        if(gestureState.dx > this.state.threshold &&
-          gestureState.y0 < this.state.middle) {
+        let xVal = gestureState.dx;
+        let yVal = gestureState.y0;
+        let xThresh = this.state.xThresh;
+        let yThresh = this.state.yThresh;
+
+        if(xVal > xThresh && yVal < yThresh) {
           this.animateRotation_(1);
-        } else if(gestureState.dx > this.state.threshold &&
-          gestureState.y0 >= this.state.middle) {
+        } else if(xVal > xThresh && yVal >= yThresh) {
           this.animateRotation_(0);
-        } else if (gestureState.dx < -this.state.threshold &&
-          gestureState.y0 < this.state.middle) {
+        } else if (xVal < -xThresh && yVal < yThresh) {
           this.animateRotation_(0);
-        } else if (gestureState.dx < -this.state.threshold &&
-          gestureState.y0 >= this.state.middle) {
+        } else if (xVal < -xThresh && yVal >= yThresh) {
           this.animateRotation_(1);
         } else {
           this.animateRotation_(0.5);
@@ -85,31 +88,30 @@ export default class Card extends Component {
   /**
    * Fire animation for rotating the card.
    * @param  {!Number} val Corresponding number value for animation.
+   * @private
    */
   animateRotation_(val) {
     Animated.timing(
       this.state.rotate,
-      {
-        toValue: val,
-        easing: Easing.elastic(2),
-        duration: 130,
-      }
+      { toValue: val, easing: Easing.elastic(2), duration: 170 }
     ).start();
   }
 
   /**
    * Animate initial default values and set up
    * scale animation for pan movement.
+   * @private
    */
-  animateAndSetPanValues_() {
+  animateResponse_() {
     // Set the initial value to the current state
     // and animate the card scale animation.
-    this.state.pan.setOffset(
-      {x: this.state.pan.x._value, y: this.state.pan.y._value}
-    );
-    this.state.pan.setValue(
-      {x: 0,y: 0}
-    );
+    this.state.pan.setOffset({
+      x: this.state.pan.x._value,
+      y: this.state.pan.y._value
+    });
+
+    this.state.pan.setValue({x: 0,y: 0});
+
     Animated.spring(
       this.state.scale,
       { toValue: 1.1, friction: 3 }
@@ -120,6 +122,7 @@ export default class Card extends Component {
    * Fire animation according to x and y parameters given.
    * @param  {!Number} x X position to animate to.
    * @param  {!number} y Y position to animate to.
+   * @private
    */
   animateRelease_(x, y) {
     // Flatten the offset to avoid erratic behavior.
@@ -130,6 +133,7 @@ export default class Card extends Component {
       this.state.scale,
       { toValue: 1, friction: 3 }
     ).start();
+
     Animated.spring(
       this.state.pan,
       { toValue: {x: x, y: y}, tension: 15 }
@@ -139,37 +143,47 @@ export default class Card extends Component {
   /**
    * Fire release animations according to logic.
    * @param  {!Number} pos Current position of pan.
+   * @private
    */
   gaugeRelease_(pos) {
+    // Define x threshold and current x position.
+    let x = pos.dx;
+    let xThresh = this.state.xThresh;
+
     // Gauge release movement threshold and animate accordingly.
-    if(pos.dx <= this.state.threshold && pos.dx >= -this.state.threshold) {
+    if(x <= xThresh && x >= -xThresh) {
       this.animateRelease_(0, 0);
-    } else if (pos.dx > this.state.threshold) {
+    } else if (x > xThresh) {
       this.animateRelease_(500, 350);
-    } else if (pos.dx < -this.state.threshold) {
+    } else if (x < -xThresh) {
       this.animateRelease_(-500, 350);
     }
   }
 
   /**
    * Render card component with animation and styles given.
+   * @public
    */
   render() {
     // Define pan, scale and transform styles.
     let { pan, scale } = this.state;
     let [translateX, translateY] = [pan.x, pan.y];
 
-    // Switch rotation based on state rotate property.
+    // Use interpolation function to switch degree of rotation
+    // based on the inputRange defined.
     const rotate = this.state.rotate.interpolate({
       inputRange: [0, 0.5, 1],
       outputRange: ['-25deg', '0deg', '25deg'],
       extrapolate: 'clamp'
     });
 
-    // Define styles for card to pass in.
-    let cardStyle = {transform: [{translateX}, {translateY}, {rotate}, {scale}]};
+    // Define final styles object to pass into card.
+    let cardStyle = {
+      transform: [{translateX}, {translateY}, {rotate}, {scale}]
+    };
 
     return (
+      // Render swipe and animation supported card component.
       <Animated.View style={[styles.cardLayout, {...cardStyle}]} {...this._panResponder.panHandlers}>
         <Image source={this.props.src} />
         <Text style={styles.cardLabel}>{this.props.name}</Text>
